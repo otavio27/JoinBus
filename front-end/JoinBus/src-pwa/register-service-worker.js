faca1a54d0132040eeaf3064879b98c85b9d2679
otavio27 <owtechwebserver@gmail.com>
2023-04-04 00:07:15 -0300

    fix bug

--- front-end/JoinBus/src-pwa/register-service-worker.js
+++ front-end/JoinBus/src-pwa/register-service-worker.js
@@ -1,4 +1,4 @@
-import { register } from 'register-service-worker'
+import { register } from "register-service-worker";
 
 // The ready(), registered(), cached(), updatefound() and updated()
 // events passes a ServiceWorkerRegistration instance in their arguments.
@@ -11,31 +11,45 @@ register(process.env.SERVICE_WORKER_FILE, {
 
   // registrationOptions: { scope: './' },
 
-  ready (/* registration */) {
+  ready(/* registration */) {
     // console.log('Service worker is active.')
   },
 
-  registered (/* registration */) {
+  registered(/* registration */) {
     // console.log('Service worker has been registered.')
   },
 
-  cached (/* registration */) {
+  cached(/* registration */) {
     // console.log('Content has been cached for offline use.')
   },
 
-  updatefound (/* registration */) {
+  updatefound(/* registration */) {
     // console.log('New content is downloading.')
   },
 
-  updated (/* registration */) {
-    // console.log('New content is available; please refresh.')
+  updated(/* registration */) {
+    register(process.env.SERVICE_WORKER_FILE, {
+      updated(/* registration */) {
+        Notify.create({
+          color: "negative",
+          icon: mdiCached,
+          message: "Atualização disponível. Atualize a página.",
+          timeout: 0,
+          multiLine: true,
+          position: "top",
+          actions: [
+            /*...*/
+          ],
+        });
+      },
+    });
   },
 
-  offline () {
+  offline() {
     // console.log('No internet connection found. App is running in offline mode.')
   },
 
-  error (/* err */) {
+  error(/* err */) {
     // console.error('Error during service worker registration:', err)
-  }
-})
+  },
+});
