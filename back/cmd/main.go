1e7126bcf87522fcc77adc777d659f9cda872ead
otavio27 <owtechwebserver@gmail.com>
2023-07-30 21:45:44 -0300

    vamos para mais um teste

--- back/cmd/main.go
+++ back/cmd/main.go
@@ -32,7 +32,7 @@ func main() {
 	errHandler := middlewares.NewErrorHandler(logger)
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://owtechsystems.com/, https://*.owtechsystems.com/",
+		AllowOrigins: "http://owtechsystems.com/, http://*.owtechsystems.com/",
 		AllowHeaders: "Origin, Content-Type, Accept",
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS, HEAD",
 	}))
