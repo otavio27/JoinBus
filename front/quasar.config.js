e82449b623600b27e298c3dc1ee86065d2cec2c3
otavio27 <owtechwebserver@gmail.com>
2023-08-02 16:08:56 -0300

    fix

--- front/quasar.config.js
+++ front/quasar.config.js
@@ -81,9 +81,9 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-devServer
     devServer: {
       server: {
-        type: "http",
+        type: "https",
       },
-      port: 80,
+      port: 443,
       open: false, // opens browser window automatically
     },
 
