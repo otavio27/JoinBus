4390561803c4bad83d89d154fa40b42560c45d2b
otavio27 <owtechwebserver@gmail.com>
2023-08-04 13:04:27 -0300

    add publicPath

--- front/quasar.config.js
+++ front/quasar.config.js
@@ -52,7 +52,7 @@ module.exports = configure(function (ctx) {
       vueRouterMode: "history", // available values: 'hash', 'history'
 
       // transpile: false,
-      // publicPath: '/',
+      publicPath: '/app/bus',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
