bf53d35c9c6da71700fb66d4e5570e52d41e5865
otavio27 <owtechsystems@gmail.com>
2023-08-08 15:02:30 -0300

    fix

--- back/buildBack.sh
+++ back/buildBack.sh
@@ -22,17 +22,17 @@ if [ $patch -eq 10 ]; then
     fi
 fi
 
-# Atualiza a tag no arquivo docker-compose.yml
 new_tag="$major.$minor.$patch"
 
+# Atualiza a tag no arquivo docker-compose.yml
 sed -i "s|otavio27/joinbus:${TG}|otavio27/joinbus:${new_tag}|g" docker-compose.yml
 
 docker ps >/dev/null 2>&1
 if [ $? -eq 1 ]; then
-    #O comando sudo setfacl --modify user:otavio:rw /var/run/docker.sock 
-    #atribui permissões ACL (Access Control List) ao arquivo /var/run/docker.sock 
-    #para permitir que o usuário otavio tenha permissões de leitura e gravação (read-write) sobre ele. 
-    #Isso permite que o usuário otavio acesse o socket do Docker 
+    #O comando sudo setfacl --modify user:otavio:rw /var/run/docker.sock
+    #atribui permissões ACL (Access Control List) ao arquivo /var/run/docker.sock
+    #para permitir que o usuário otavio tenha permissões de leitura e gravação (read-write) sobre ele.
+    #Isso permite que o usuário otavio acesse o socket do Docker
     #e execute comandos Docker sem a necessidade de privilégios de superusuário.
     sudo setfacl --modify user:otavio:rw /var/run/docker.sock
     docker build -t otavio27/joinbus:${new_tag} . && clear
